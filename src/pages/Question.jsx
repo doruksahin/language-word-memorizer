5b09170981f423c936a3ae80bacb64d02cc768d5
doruksahin <doruksahin@github.com>
2022-11-11 01:13:21 +0300

    biggest checkpoint

--- src/pages/Question.jsx
+++ src/pages/Question.jsx
@@ -7,7 +7,12 @@ import QuestionsAPI from "../services/QuestionsAPI";
 
 export default function Question() {
   const [questions, setQuestions] = useState();
-  const [progress, setProgress] = useState(0);
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [choosedAnswer, setChoosedAnswer] = useState(null);
+
+  useEffect(() => {
+    console.log("question", questions);
+  }, [questions]);
 
   function extractExcel(file) {
     const data = new FormData();
@@ -17,74 +22,178 @@ export default function Question() {
 
   function getQuestionsCb(isSuccess, data) {
     console.log("data", data);
-    setQuestions(data);
+    setQuestions(
+      data.map((datum) => {
+        datum.isSuccess = null;
+        return datum;
+      })
+    );
+  }
+
+  function checkQuestionAnswer() {
+    if (choosedAnswer) {
+      if (choosedAnswer === questions[questionIndex].answer) {
+        setQuestions((current) => {
+          current[questionIndex].isSuccess = true;
+          return current;
+        });
+      } else {
+        setQuestions((current) => {
+          current[questionIndex].isSuccess = false;
+          return current;
+        });
+      }
+    }
+  }
+
+  function increaseProgress() {
+    if (questionIndex < questions.length - 1) {
+      checkQuestionAnswer();
+      setQuestionIndex((prev) => prev + 1);
+      setChoosedAnswer(null);
+    }
+  }
+
+  function decreaseProgress() {
+    if (questionIndex > 0) {
+      checkQuestionAnswer();
+      setQuestionIndex((prev) => prev - 1);
+      setChoosedAnswer(null);
+    }
   }
 
   return (
     <div className="flex items-center flex-col bg-blue-300 w-screen h-screen">
       {questions ? (
         <QuestionCard
-          totalQuestion={questions.length}
-          currentQuestion={progress}
+          question={questions[questionIndex]}
+          increaseProgress={increaseProgress}
+          decreaseProgress={decreaseProgress}
+          choosedAnswer={choosedAnswer}
+          setChoosedAnswer={setChoosedAnswer}
         />
       ) : undefined}
-      <Progress questions={questions} currentQuestion={progress} />
-      <FileUploader className={"mt-10"} handleFileUpload={extractExcel}>
-        <img className="w-6 h-6 mr-2" src={excelSvg} />
-      </FileUploader>
+      <ProgressBar questionIndex={questionIndex} questions={questions} />
+      {!questions ? (
+        <FileUploader className={"mt-10"} handleFileUpload={extractExcel}>
+          <img className="w-6 h-6 mr-2" src={excelSvg} />
+        </FileUploader>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
 
-function QuestionCard({ totalQuestion, currentQuestion }) {
+//TODO: What if i wanted to put progress in here?
+function QuestionCard({
+  question,
+  increaseProgress,
+  decreaseProgress,
+  choosedAnswer,
+  setChoosedAnswer,
+}) {
   return (
     <div className="w-screen sm:max-w-min h-min p-6 items-center mt-4 rounded-xl bg-white shadow-lg">
-      <div className="text-center text-xl font-bold">Cooking</div>
+      <div className="text-center text-xl font-bold">{question.word}</div>
       <div className="flex flex-col sm:flex-row justify-center mx-24 mt-4 items-center gap-2 sm:gap-12">
-        <Options />
+        <OptionsRadio
+          options={question.options}
+          choosedAnswer={choosedAnswer}
+          setChoosedAnswer={setChoosedAnswer}
+        />
       </div>
       <div className="flex mt-6 justify-between">
-        <img className="w-12 h-12" src={leftArrow}></img>
-        <div className="mt-8">1/10</div>
-        <img className="w-12 h-12" src={rightArrow}></img>
+        <img
+          className="w-12 h-12 cursor-pointer"
+          src={leftArrow}
+          onClick={decreaseProgress}
+        ></img>
+        <img
+          className="w-12 h-12 cursor-pointer"
+          src={rightArrow}
+          onClick={increaseProgress}
+        ></img>
       </div>
     </div>
   );
 }
 
-function Options() {
-  const options = [
-    "Option1",
-    "Option2",
-    "Option3",
-    "Option3",
-    "Option3",
-    "Option3",
-  ];
-  const listOptions = options.map((item, index) => (
+function OptionsKahoot({ options }) {
+  const listOptions = options.map((option, index) => (
     <div key={index} className="whitespace-nowrap">
-      <input className="mr-1" type="radio" value={item} />
-      {item}
+      <div className="">{option}</div>
     </div>
   ));
 
   return listOptions;
 }
 
-function Progress({ questions }) {
-  const progresses = [null, null, null, null, null, true, false];
+function OptionsRadio({ options, choosedAnswer, setChoosedAnswer }) {
+  const listOptions = options.map((option, index) => (
+    <div key={index} className="whitespace-nowrap">
+      <input
+        className="mr-1 cursor-pointer"
+        name="answer"
+        type="radio"
+        value={option}
+        checked={choosedAnswer === option}
+        onClick={() => setChoosedAnswer(option)}
+      />
+      {option}
+    </div>
+  ));
+
+  return listOptions;
+}
+
+function ProgressBar({ questionIndex, questions }) {
   if (questions) {
+    const questionsToBeRendered = questions.map((question, index) => (
+      <ProgressItem
+        questionIndex={questionIndex}
+        isSuccess={question.isSuccess}
+        index={index}
+      />
+    ));
+
     return (
       <div className="flex flex-col items-center mt-4">
         Progress
         <div className="w-40 flex-wrap flex self-start bg-blue-200">
-          {questions.map((question) => (
-            <div className="border border-red-400 w-[20%] h-8"></div>
-          ))}
+          {questionsToBeRendered}
         </div>
       </div>
     );
   } else {
     return <></>;
   }
+}
+
+function ProgressItem({ questionIndex, isSuccess, index }) {
+  var progressCircle;
+  if (questionIndex == index) {
+    progressCircle = <div className="bg-white h-4 w-4 rounded-full"></div>;
+  }
+  if (isSuccess === true) {
+    return (
+      <div className="flex items-center justify-center bg-green-400 w-[20%] h-8">
+        {progressCircle}
+      </div>
+    );
+  } else if (isSuccess === false) {
+    return (
+      <div className="flex items-center justify-center bg-red-400 w-[20%] h-8">
+        {progressCircle}
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex items-center justify-center border border-white bg-black w-[20%] h-8">
+        {progressCircle}
+      </div>
+    );
+  }
+
+  return { progressBox };
 }
